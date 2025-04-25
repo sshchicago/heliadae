839a5cb1046f68c37ece55032868e0b9f830d8c8
Adam Kadzban <adam.kadzban@gmail.com>
2025-04-24 21:31:44 -0500

    Run GPS and Radiacode as separate services (and don't pull from RC buffer) so they're each more resiliant

--- gps.py
+++ gps.py
@@ -17,53 +17,40 @@ class Gps():
     Contains a PySerial UART connection, and a I/O thread.
     Also includes functions to configure the GPS, and generate "UBX" messages.
     """
-    mainLogFile = None
-    default_timeout = 0.1 # Serial port read timeout. Should be quite low.
+    main_log_file = None
+    data_log_file = None
     port = None
+    default_timeout = 0.1
 
     ubx_write_queue = None
     ubx_read_queue = None
 
     io_thread = None
 
-    def __init__(self, mainLogFile):
+    def __init__(self, main_log_file, data_log_file):
         """
         Configure the GPS device and initialize queues, and start the I/O thread.
         """
-        self.mainLogFile = mainLogFile
-        start = datetime.now()
-        cwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-
-        base_directory = f'{cwd}/logs'
-        os.makedirs(base_directory, exist_ok=True)
-        logFileName = f'{base_directory}/gps-{start}.log'
-        with open(logFileName, 'w') as logFile:
-            logFile.write(f'### Beginning at {start}\n')
-
+        self.main_log_file = main_log_file
+        self.data_log_file = data_log_file
+        
         self.port = serial.Serial('/dev/ttyS0', 9600, timeout=self.default_timeout)
         self.ubx_write_queue = queue.Queue()
         self.ubx_read_queue = queue.Queue()
-        self.io_thread = threading.Thread(target=self.__io_thread, args=[logFileName], daemon=True)
+        self.io_thread = threading.Thread(target=self.__io_thread, daemon=True)
         self.io_thread.start()
         time.sleep(1)
         self._configure_output_messages()
         time.sleep(1)
         self._enable_flight_mode()
-        self.log("Done with init")
-
-
-    def isThreadAlive(self):
-        """
-        Used by main.py to see if the background thread crashed.
-        """
-        return self.io_thread.is_alive()
+        log(main_log_file, "GPS initialized")
 
 
     def _configure_output_messages(self):
         """
         Disables NMEA sentences with CFG-PRN: GLL, GSA, GSV, RMC, VTG (id 1 to 5)
         """
-        self.log("Configuring NMEA sentence output")
+        log(self.main_log_file, "Configuring NMEA sentence output")
         ubx_cfg_class = 0x06
         ubx_cfg_msg = 0x01
         for index in range(1, 6):
@@ -72,8 +59,8 @@ class Gps():
             payload += bytearray.fromhex("00 00 00 00 00 01")
             ack_ok = self._send_and_confirm_ubx_packet(ubx_cfg_class, ubx_cfg_msg, payload)
             if not ack_ok:
-                raise Exception("Failed to configure output message id {}".format(index))
-        self.log("NMEA sentence output configured.")
+                raise Exception(f"Failed to configure output message id {index}")
+        log(self.main_log_file, "NMEA sentence output configured.")
 
 
     def _enable_flight_mode(self):
@@ -87,14 +74,14 @@ class Gps():
             https://github.com/PiInTheSky/pits/blob/master/tracker/gps.c#L423
         Reference: https://content.u-blox.com/sites/default/files/products/documents/u-blox8-M8_ReceiverDescrProtSpec_UBX-13003221.pdf?utm_content=UBX-13003221
         """
-        self.log("Enabling flight mode")
+        log(self.main_log_file, "Enabling flight mode")
         cfg_nav5_class_id = 0x06
         cfg_nav5_message_id = 0x24
         payload = bytearray.fromhex("FF FF 06 03 00 00 00 00 10 27 00 00 05 00 FA 00 FA 00 64 00 2C 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
         ack_ok = self._send_and_confirm_ubx_packet(cfg_nav5_class_id, cfg_nav5_message_id, payload)
         if not ack_ok:
             raise Exception("Failed to configure GPS for flight mode.")
-        self.log("Flight mode enabled.")
+        log(self.main_log_file, "Flight mode enabled.")
 
 
     def _reboot(self):
@@ -155,7 +142,7 @@ class Gps():
             raise Exception("ubx_read_queue must be empty before calling this function")
         send_packet = self._ubx_assemble_packet(class_id, message_id, payload)
         self.ubx_write_queue.put(send_packet)
-        self.log("UBX packet built: {}".format(send_packet))
+        log(self.main_log_file, f"UBX packet built: {send_packet}")
 
         expected_ack = self._ubx_assemble_packet(0x05, 0x01, bytearray((class_id, message_id)))
 
@@ -166,42 +153,41 @@ class Gps():
             if self.ubx_read_queue.qsize() > 0:
                 ack = self.ubx_read_queue.get()
                 if ack == expected_ack:
-                    self.log("UBX packet ACKd: {}".format(ack))
+                    log(self.main_log_file, f"UBX packet ACKd: {ack}")
                     return True
                 elif ack[2:3] == bytearray.fromhex("05 01"):
-                    self.log("UBX-NAK packet! {}".format(ack))
+                    log(self.main_log_file, f"UBX-NAK packet! {ack}")
                     return False
                 else:
-                    self.log("Unknown UBX reply: {}".format(ack))
-                    self.log("Looking for      : {}".format(expected_ack))
+                    log(self.main_log_file, f"Unknown UBX reply: {ack}")
+                    log(self.main_log_file, f"Looking for      : {expected_ack}")
                 return True
-        self.log("UBX packet sent without ACK! This is bad.")
+        log(self.main_log_file, "UBX packet sent without ACK! This is bad.")
         return False
 
 
-    def __io_thread(self, logFileName):
+    def __io_thread(self):
         """
         Singleton thread which will run indefinitely, reading and
         writing between the gps serial and {read,write}_queue.
 
         Do not invoke directly, this method never returns.
         """
-        self.log("I/O thread started")
-        with open(logFileName, 'a') as logFile:
-            while True:
-                while self.ubx_write_queue.qsize() > 0:
-                    to_write = self.ubx_write_queue.get()
-                    to_write_type = type(to_write)
-                    if to_write_type == str:
-                        to_write = to_write.encode('utf-8')
-                    self.log("UBX packet write {}: {}".format(to_write_type, to_write))
-                    self.port.write(to_write)
-                got_some_data = self.__read(logFile)
-                if not got_some_data:
-                    time.sleep(0.1)
+        log(self.main_log_file, "I/O thread running")
+        while True:
+            while self.ubx_write_queue.qsize() > 0:
+                to_write = self.ubx_write_queue.get()
+                to_write_type = type(to_write)
+                if to_write_type == str:
+                    to_write = to_write.encode('utf-8')
+                log(self.main_log_file, f"UBX packet write {to_write_type}: {to_write}")
+                self.port.write(to_write)
+            got_some_data = self.__read()
+            if not got_some_data:
+                time.sleep(0.1)
 
 
-    def __read(self, logFile):
+    def __read(self):
         """
         Reads a from the GPS serial port.
         Interprets between UBX and NMEA packets and places into appropriate queues.
@@ -213,7 +199,10 @@ class Gps():
         if waiting == 0:
             return False
         first_byte = self.port.read()
-        if first_byte == b'\xb5': # looks like a UBX proprietary packet
+        if first_byte == b'\x00': # garbage
+            log(self.main_log_file, "Empty bytes received")
+            return False
+        elif first_byte == b'\xb5': # looks like a UBX proprietary packet
             self.port.timeout = 10
             remaining_header = self.port.read(3)
             length_bytes = self.port.read(2)
@@ -222,7 +211,7 @@ class Gps():
             self.port.timeout = self.default_timeout
             ubx_packet = first_byte + remaining_header + length_bytes + remaining_packet
             self.ubx_read_queue.put(ubx_packet)
-            self.log("UBX raw packet received: {}".format(ubx_packet))
+            log(self.main_log_file, f"UBX raw packet received: {ubx_packet}")
             return True
         else:
             line = self.port.readline()
@@ -230,32 +219,42 @@ class Gps():
         try:
             ascii_line = line.decode('ascii')
         except UnicodeDecodeError as exception:
-            self.log(exception)
-            self.log("Reply string decode error on: {}".format(line))
-            return False
+            log(self.main_log_file, exception)
+            log(self.main_log_file, f"Reply string decode error on: {line}")
+            raise Exception("Failed to decode string from GPS. Did you get a GNTXT? It just restarted!")
         if ascii_line[0] != "$":
-            self.log("Non-dollar line")
+            log(self.main_log_file, "Non-dollar line")
             return False
         # We actually have a NMEA sentence!
-        self.log("NMEA (buf={}) raw line: {}".format(waiting, line))
+        log(self.main_log_file, f"NMEA (buf={waiting}) raw line: {line}")
         try:
             nmea_line = pynmea2.parse(ascii_line.strip(), check=True)
         except pynmea2.nmea.ParseError as exception:
-            self.log(exception)
+            log(self.main_log_file, exception)
             return False
         if isinstance(nmea_line, pynmea2.types.talker.GGA):
-            logFile.write(repr(nmea_line) + "\n")
-            logFile.flush()
+            log(self.data_log_file, repr(nmea_line))
         else:
-            self.log("Unhandled message type received: {}".format(nmea_line))
+            log(self.main_log_file, f"Unhandled message type received: {nmea_line}")
         return True
 
-    def log(self, message):
-        self.mainLogFile.write(f'[{datetime.now()}]: GPS: {message}\n')
-        self.mainLogFile.flush()
+def log(log_file, message):
+    with open(log_file, 'a') as logger:
+        logger.write(f'[{datetime.now()}]: {message}\n')
+
+def main():
+    start = datetime.now()
+    home = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    log_directory = f'{home}/logs'
+    os.makedirs(log_directory, exist_ok=True)
+    main_log_file = f'{log_directory}/gps-main-{start.strftime("%Y-%m-%dT%H:%M:%S")}.log'
+    data_log_file = main_log_file.replace("main", "data")
+    
+    try:
+        _ = Gps(main_log_file, data_log_file)
+        _ = input("Press Enter or Ctrl+c to exit...")
+    except KeyboardInterrupt:
+        log(main_log_file, "Ctrl+c detected, shutting down.")
 
 if __name__ == "__main__":
-    with open(f'main-gps-{datetime.now()}.log', 'w') as logFile:
-        print(f"GPS: [{datetime.now()}]: Starting GPS...")
-        gps = Gps(logFile)
-        input(f"GPS: [{datetime.now()}]: Running. Press Enter to exit.")
+    main()
--- rc.py
+++ rc.py
@@ -1,89 +1,48 @@
-"""
-Radiacode library / logger.
-"""
 import os
-import threading
 import time
 from datetime import datetime
 
 import radiacode as rcCore
 
-class Rc():
-    """
-    Encapsulates the Radiacode 10X detector.
-    Assumes a USB connection.
-    """
-    mainLogFile = None
-    io_thread = None
+def log(log_file, message):
+    with open(log_file, 'a') as logger:
+        logger.write(f'[{datetime.now()}]: {message}\n')
 
-    def __init__(self, mainLogFile):
-        """
-        Initialize the Radiacode 10X to log to disk.
-        """
-        self.mainLogFile = mainLogFile
+def main():
+    start = datetime.now()
+    home = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    log_directory = f'{home}/logs'
+    os.makedirs(log_directory, exist_ok=True)
+    main_log_file = f'{log_directory}/rc-main-{start.strftime("%Y-%m-%dT%H:%M:%S")}.log'
+    data_log_file = main_log_file.replace("main", "data")
 
-        device = rcCore.RadiaCode()
-        a0, a1, a2 = device.energy_calib()
-        start = datetime.now()
-        cwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    log(main_log_file, "Connecting to Radiacode...")
+    device = rcCore.RadiaCode()
+    log(main_log_file, "Connected!")
+    a0, a1, a2 = device.energy_calib()
 
-        base_directory = f'{cwd}/logs'
-        os.makedirs(base_directory, exist_ok=True)
-        logFileName = f'{base_directory}/rc-{start}.log'
-        self.log(f'Logging to {logFileName}')
-
-        with open(logFileName, 'w') as logFile:
-            logFile.write(f'### Beginning at {start}.\n')
-            logFile.write(f'### Serial number: {device.serial_number()}.\n')
-            logFile.write(f'### Firmware: {device.fw_version()}\n')
-            logFile.write(f'### Energy Calibration constants: {a0}, {a1}, {a2}.\n')
-
-        self.log("Resetting dose and spectrum")
-        device.dose_reset()
-        device.spectrum_reset()
+    log(main_log_file, f'Serial number: {device.serial_number()}.')
+    log(main_log_file, f'Firmware: {device.fw_version()}.')
+    log(main_log_file, f'Energy Calibration constants: {a0}, {a1}, {a2}.')
+    log(main_log_file, f'Logging data to {data_log_file}.')
         
-        self.io_thread = threading.Thread(target=self.__io_thread, args=[device, logFileName, start], daemon=True)
-        self.io_thread.start()
-        self.log("Done with init")
+    #log(main_log_file, "Resetting dose and spectrum")
+    #device.dose_reset()
+    #device.spectrum_reset()
 
-
-    def isThreadAlive(self):
-        """
-        Used by main.py to see if the background thread crashed.
-        """
-        return self.io_thread.is_alive()
-
-
-    def __io_thread(self, device, logFileName, start):
-        """
-        Do not invoke directly, this method never returns.
-        """
-        self.log("Radiacode i/o thread running")
-        logFile = open(logFileName, 'a')
+    try:
         while True:
-            self.log("Logging buffer")
-            for data in device.data_buf():
-                msg = data.dt.isoformat() + " " + repr(data) + "\n"
-                logFile.write(msg)
-                logFile.flush()
-            
-            self.log("Logging spectrum")
+            #log(main_log_file, "Logging buffer")
+            #for data in device.data_buf():
+            #    msg = data.dt.isoformat() + " " + repr(data) + "\n"
+            #    log(data_log_file, msg)
+            log(main_log_file, "Logging spectrum")
             spectrum = device.spectrum()
-            newTime = start + spectrum.duration
-            
-            msg = newTime.isoformat() + " " + repr(spectrum) + "\n"
-            logFile.write(msg)
-            logFile.flush()
-
-            self.log("Sleeping")
+            log(data_log_file, repr(spectrum) + "\n")
+            log(main_log_file, "Sleeping")
             time.sleep(5)
+    except KeyboardInterrupt:
+        log(main_log_file, "Ctrl+c detected, shutting down.")
 
-    def log(self, message):
-        self.mainLogFile.write(f'[{datetime.now()}]: RC: {message}\n')
-        self.mainLogFile.flush()
-    
 if __name__ == "__main__":
-    with open(f'main-rc-{datetime.now()}.log', 'w') as logFile:
-        print("RC: Starting Radiacode...")
-        rc = Rc(logFile)
-        input("RC: Running. Press Enter to exit.")
+    main()
